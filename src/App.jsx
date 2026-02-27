8f91662b8d8d0cfb1610662834fde6fc518a0912
amirrezahashemi403-cloud <amirrezahashemi403@gmail.com>
2026-02-27 16:13:24 +0330

    Register page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RoleSelect from './pages/RoleSelect';
 import LoginForm from './pages/LoginForm';
+import Register from './pages/Register';
 import './App.css';
 
 function App() {
@@ -9,11 +10,9 @@ function App() {
     <BrowserRouter>
       <div className="app">
         <Routes>
-          {/* صفحه انتخاب نقش (همون صفحه اول) */}
           <Route path="/" element={<RoleSelect />} />
-          
-          {/* صفحه فرم لاگین - با پارامتر نقش */}
           <Route path="/login/:role" element={<LoginForm />} />
+          <Route path="/register/:role" element={<Register />} />
         </Routes>
       </div>
     </BrowserRouter>
